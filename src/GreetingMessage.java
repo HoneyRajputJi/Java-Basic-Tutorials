c5c5c0f201dafaa87410d3db9244d9f738f9c40f
Harender Rawat <honeyrajput962@gmail.com>
2025-04-01 16:54:17 +0530

    sout statement in greetings file

--- src/GreetingMessage.java
+++ src/GreetingMessage.java
@@ -5,6 +5,7 @@ public class GreetingMessage {
 
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter your Name");
+        System.out.println("asdfghj");
         String name=sc.nextLine();
         System.out.println("Hello" +" "+ name + " Welcome to your program");
 
