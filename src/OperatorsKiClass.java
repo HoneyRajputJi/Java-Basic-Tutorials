72b02186667278da12cccb382d0ec516119e1c8d
Harender Rawat <honeyrajput962@gmail.com>
2025-04-06 12:40:51 +0530

    Operators ki Class

---
+++ src/OperatorsKiClass.java
@@ -0,0 +1,26 @@
+public class OperatorsKiClass {
+     public static void main(String[] args){
+         int a=4000;
+         int b=20;
+         int c=a+b;
+         int d=a-b;
+         int e =a*b;
+         int f=a/b;
+
+         int g= 64 % b; // it is called modulo operator. It will provide the remainder. as 64 will get dived by ba d remainder will be 4
+         System.out.println(g);
+
+         int h =20;
+         h+=5; // it means add 5 in H
+         System.out.println(h);
+         h*=5; //it means h multiply by 5
+         System.out.println(h);
+         System.out.println(64<67);
+         System.out.println(a>b);
+         System.out.println(64>5 && 64<8 ); //&& operator there is 2 conditions, both should be true then it will print true otherwise it will print false
+         System.out.println(64>5 || 64<18); //OR operator if any one condition is true then it will print true
+         System.out.println(); //bitwise
+
+     }
+
+}
--- src/Scanner_5th.java
+++ src/Scanner_5th.java
@@ -25,6 +25,7 @@ public class Scanner_5th {
 
 
 
+
     }
 
 }
